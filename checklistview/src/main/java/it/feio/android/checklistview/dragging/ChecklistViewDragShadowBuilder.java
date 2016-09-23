8928dc6fff767c29c4b4a65a1e8ac2155d5a5248
Federico Iosue <federico.iosue@gmail.com>
2016-09-23 17:58:05 +0200

    Refactoring and minor changes

--- checklistview/src/main/java/it/feio/android/checklistview/dragging/ChecklistViewDragShadowBuilder.java
+++ checklistview/src/main/java/it/feio/android/checklistview/dragging/ChecklistViewDragShadowBuilder.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.View.DragShadowBuilder;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class ChecklistViewDragShadowBuilder extends DragShadowBuilder {
+class ChecklistViewDragShadowBuilder extends DragShadowBuilder {
 
 
     ChecklistViewDragShadowBuilder(View view) {
