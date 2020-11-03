14ce642f187d17031055d8e2f36649b45e1095a7
Federico Iosue <federico@iosue.it>
2020-11-03 12:07:59 +0100

    Fixed instrumentation test

--- checklistview/src/androidTest/java/ViewGroupTest.java
+++ checklistview/src/androidTest/java/ViewGroupTest.java
@@ -1,47 +1,50 @@
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import android.animation.LayoutTransition;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.InstrumentationTestCase;
-import android.test.suitebuilder.annotation.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
 import it.feio.android.checklistview.dragging.ChecklistViewItemOnDragListener;
 import it.feio.android.checklistview.models.CheckListView;
 import it.feio.android.checklistview.models.CheckListViewItem;
+import java.lang.ref.WeakReference;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
 @RunWith(AndroidJUnit4.class)
-@SmallTest
-public class ViewGroupTest extends InstrumentationTestCase {
+public class ViewGroupTest {
 
-  private Context context;
+  private WeakReference<Context> context;
   private CheckListView checkListView;
   private CheckListViewItem checkListViewItem;
 
 
   @Before
-  protected void setUp () {
-    context = getInstrumentation().getContext();
+  public void setUp () {
+    context = new WeakReference<>(getInstrumentation().getContext());
     checkListView = new CheckListView(context);
     checkListViewItem = new CheckListViewItem(context, false, false);
   }
-
 
   @Test
   public void testChildAddRemoval () {
     checkListView.addView(checkListViewItem);
     assertTrue(checkListView.indexOfChild(checkListViewItem) >= 0);
-    assertTrue(checkListViewItem.getParent().equals(checkListView));
+    assertEquals(checkListViewItem.getParent(), checkListView);
 
     checkListView.removeView(checkListViewItem);
     assertFalse(checkListView.indexOfChild(checkListViewItem) >= 0);
-    assertFalse(checkListView.equals(checkListViewItem.getParent()));
-    assertTrue(checkListViewItem.getParent() == null);
+    assertNotEquals(checkListView, checkListViewItem.getParent());
+    assertNull(checkListViewItem.getParent());
   }
-
 
   @TargetApi(Build.VERSION_CODES.HONEYCOMB)
   @Test
@@ -51,18 +54,16 @@ public class ViewGroupTest extends InstrumentationTestCase {
     CheckListViewItem checkListViewItem1 = new CheckListViewItem(context, false, false);
     checkListView.addView(checkListViewItem);
     checkListView.addView(checkListViewItem1);
-    assertTrue(checkListView.indexOfChild(checkListViewItem) == 0);
-    assertTrue(checkListView.indexOfChild(checkListViewItem1) == 1);
-    assertTrue(checkListViewItem.getParent().equals(checkListView));
-    assertTrue(checkListViewItem1.getParent().equals(checkListView));
+    assertEquals(0, checkListView.indexOfChild(checkListViewItem));
+    assertEquals(1, checkListView.indexOfChild(checkListViewItem1));
+    assertEquals(checkListViewItem.getParent(), checkListView);
+    assertEquals(checkListViewItem1.getParent(), checkListView);
 
     ChecklistViewItemOnDragListener checklistViewOnTouchListener = new ChecklistViewItemOnDragListener();
     checkListView.setOnDragListener(checklistViewOnTouchListener);
     checklistViewOnTouchListener.moveView(checkListViewItem, checkListViewItem1);
-    assertTrue(checkListView.indexOfChild(checkListViewItem) == 1);
-    assertTrue(checkListView.indexOfChild(checkListViewItem1) == 0);
-
-
+    assertEquals(1, checkListView.indexOfChild(checkListViewItem));
+    assertEquals(0, checkListView.indexOfChild(checkListViewItem1));
   }
 
 }
